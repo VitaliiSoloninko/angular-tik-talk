4a6baf5ca1dc7287ffce50174d22dbfa8c513190
22051988Di <vitalii.soloninko@gmail.com>
2024-10-07 13:14:57 +0200

    1.1	Backend. Connect and get users json

---
+++ src/app/data/services/profile.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  http = inject(HttpClient);
+
+  baseApiUrl = 'https://icherniakov.ru/yt-course/';
+
+  constructor() {}
+
+  getTestAccounts() {
+    return this.http.get(`${this.baseApiUrl}account/test_accounts`);
+  }
+}
