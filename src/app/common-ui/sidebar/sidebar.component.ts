c823154a092ed557f1388ffe7063eca96f4f8ae7
22051988Di <vitalii.soloninko@gmail.com>
2024-10-17 14:19:28 +0200

    5.4	SiteBar. Menu. Get data from file ts. @ngFor

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,11 +1,30 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, NgFor],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  menuItems = [
+    {
+      label: 'My page',
+      icon: 'home',
+      link: '',
+    },
+    {
+      label: 'Chats',
+      icon: 'chats',
+      link: 'chats',
+    },
+    {
+      label: 'Search',
+      icon: 'search',
+      link: 'search',
+    },
+  ];
+}
