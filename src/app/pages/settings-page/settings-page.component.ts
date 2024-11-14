78a5dcb6893ccb0ab768720a3cfe23848a038459
22051988Di <vitalii.soloninko@gmail.com>
2024-11-14 14:25:48 +0100

    7.2	Settings page. FormBuilder, all inputs + textarea

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProfileHeaderComponent } from '../../common-ui/profile-header/profile-header.component';
 
 @Component({
   selector: 'app-settings-page',
   standalone: true,
-  imports: [ProfileHeaderComponent],
+  imports: [ProfileHeaderComponent, ReactiveFormsModule],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.scss',
 })
-export class SettingsPageComponent {}
+export class SettingsPageComponent {
+  formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    username: ['', Validators.required],
+    description: [''],
+    stack: [''],
+  });
+}
