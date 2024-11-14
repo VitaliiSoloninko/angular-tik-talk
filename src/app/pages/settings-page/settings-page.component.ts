c4f159bfad07b34a6b25d2d3996021be55c16d8e
22051988Di <vitalii.soloninko@gmail.com>
2024-11-14 14:56:25 +0100

    7.4	Settings page. Get data from server (name, lastname, username)

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProfileHeaderComponent } from '../../common-ui/profile-header/profile-header.component';
+import { ProfileService } from '../../data/services/profile.service';
 
 @Component({
   selector: 'app-settings-page',
@@ -11,12 +12,22 @@ import { ProfileHeaderComponent } from '../../common-ui/profile-header/profile-h
 })
 export class SettingsPageComponent {
   formBuilder = inject(FormBuilder);
+  profileService = inject(ProfileService);
 
   form = this.formBuilder.group({
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
-    username: ['', Validators.required],
+    username: [{ value: '', disabled: true }, Validators.required],
     description: [''],
     stack: [''],
   });
+
+  constructor() {
+    effect(() => {
+      //@ts-ignore
+      this.form.patchValue(this.profileService.me());
+    });
+  }
+
+  onSave() {}
 }
