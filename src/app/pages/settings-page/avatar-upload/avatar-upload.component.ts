511dc62f8ee0d49f6931afb8acf1609f97b2c0f9
22051988Di <vitalii.soloninko@gmail.com>
2024-11-16 19:14:27 +0100

    8.5	Event, target, file, FileReader, Preview

--- src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
+++ src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SvgIconComponent } from '../../../common-ui/svg-icon/svg-icon.component';
 
 @Component({
@@ -8,4 +8,19 @@ import { SvgIconComponent } from '../../../common-ui/svg-icon/svg-icon.component
   templateUrl: './avatar-upload.component.html',
   styleUrl: './avatar-upload.component.scss',
 })
-export class AvatarUploadComponent {}
+export class AvatarUploadComponent {
+  preview = signal<string>('assets/images/placeholder.png');
+
+  fileBrowserHandler(event: Event) {
+    const file = (event.target as HTMLInputElement)?.files?.[0];
+
+    if (!file || !file.type.match('image')) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      this.preview.set(event.target?.result?.toString() ?? '');
+    };
+    reader.readAsDataURL(file);
+  }
+}
