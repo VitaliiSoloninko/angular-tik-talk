a4d0729f60425013dc4fd5d305298cc601cd7adc
22051988Di <vitalii.soloninko@gmail.com>
2024-11-18 16:41:20 +0100

    8.7	Drag and drop 2, directives DragLeave Drop HostBinding add class fileover

--- src/app/common-ui/directives/drag-and-drop.directive.ts
+++ src/app/common-ui/directives/drag-and-drop.directive.ts
@@ -1,15 +1,34 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[DragAndDrop]',
   standalone: true,
 })
 export class DragAndDropDirective {
+  @HostBinding('class.fileover')
+  fileover = false;
+
   @HostListener('dragover', ['$event'])
   onDragOver(event: DragEvent) {
     event.preventDefault();
     event.stopPropagation();
 
-    console.log(event);
+    this.fileover = true;
+  }
+
+  @HostListener('dragleave', ['$event'])
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.fileover = false;
+  }
+
+  @HostListener('drop', ['$event'])
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.fileover = false;
   }
 }
